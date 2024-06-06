15680a8d52843ad285339de7d33c5f6ec68d9d1d
Anthony Chen <cygnari@berkeley.edu>
2024-06-06 19:05:33 -0400

    Job scripts

--- job_scripts/pbs_il_time5.sh
+++ job_scripts/pbs_il_time5.sh
@@ -7,7 +7,7 @@
 #PBS -k eod
 #PBS -m abe
 #PBS -M cygnari@umich.edu
-#PBS -l select=1:ncpus=1:mpiprocs=1
+#PBS -l select=1:ncpus=2:mpiprocs=2
 #PBS -l place=group=rack
 
 export TMPDIR=/glade/derecho/scratch/$USER/temp
