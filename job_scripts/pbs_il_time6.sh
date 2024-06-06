c01294d8636578bc8e780c0594b502327e5e925c
Anthony Chen <cygnari@berkeley.edu>
2024-06-06 12:29:58 -0400

    Fix job scripts

--- job_scripts/pbs_il_time6.sh
+++ job_scripts/pbs_il_time6.sh
@@ -13,4 +13,4 @@
 export TMPDIR=/glade/derecho/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpiexec ../build/executables/single_rhs > $TMPDIR/run_out6.txt
+mpiexec ../build/executables/inverse_laplacian > $TMPDIR/run_out6.txt
