a99c9c3ae1dbdf6ee239d0d19c38693892b61bae
Anthony Chen <cygnari@berkeley.edu>
2024-06-05 22:08:26 -0400

    Job script

---
+++ job_scripts/pbs_inverse_laplacian_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N inverse_laplacian_0
+#PBS -A UMIC0093
+#PBS -l walltime=12:00:00
+#PBS -q main
+#PBS -j oe
+#PBS -k eod
+#PBS -m abe
+#PBS -M cygnari@umich.edu
+#PBS -l select=1:ncpus=1:mpiprocs=1
+#PBS -l place=group=rack
+
+export TMPDIR=/glade/derecho/scratch/$USER/temp
+mkdir -p $TMPDIR
+
+mpiexec ../build/executables/inverse_laplacian > $TMPDIR/run_out0.txt
